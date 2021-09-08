a70a6aeb7c093891f185266cb068532b92cc3b07
Jhosua Pachon <jhosuapachon@gmail.com>
2021-09-08 01:53:36 -0500

    add function to format date

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -38,3 +38,9 @@ export function extractNameProductFromArray(newArray) {
     newArray,
   };
 }
+
+export function formatDate(date) {
+  const actualDate = new Date(date);
+  const formattedDate = new Intl.DateTimeFormat("en-US").format(actualDate);
+  return formattedDate;
+}

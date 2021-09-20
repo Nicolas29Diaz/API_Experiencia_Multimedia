b48d787a236f6acccfbb416bbcebb0c9736b208f
Jhosua Pachon <jhosuapachon@gmail.com>
2021-09-20 01:22:39 -0500

    add shuffle function

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -44,3 +44,20 @@ export function formatDate(date) {
   const formattedDate = new Intl.DateTimeFormat("en-US").format(actualDate);
   return formattedDate;
 }
+
+export function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}

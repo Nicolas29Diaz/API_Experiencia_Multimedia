2a4ff8c293ff792f82074e110214cf777b818071
Jhosua Pachon <jhosuapachon@gmail.com>
2021-09-21 09:42:28 -0500

    update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import app from "./app";
 import "@babel/polyfill";
 const PORT = process.env.PORT || 4000;
 
-async function main() {
-  await app.listen(PORT, "0.0.0.0");
-  console.log(`Server on Port ${PORT}`);
+function main() {
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server on Port ${PORT}`);
+  });
 }
 
 main();

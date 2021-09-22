ccad3ddb66c8172b2a71b8ce8c71be039fe61ee3
Jhosua Pachon <jhosuapachon@gmail.com>
2021-09-22 15:30:12 -0500

    update cors

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(json());
 
-router.use(function (req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://trabajo-grado.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
@@ -29,7 +29,6 @@ router.use(function (req, res, next) {
   next();
 });
 // routes
-app.options("*", cors());
 app.use("/api/usuario", usuarioRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/profesor", profesorRoutes);
